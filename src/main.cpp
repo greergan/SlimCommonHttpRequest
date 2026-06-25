4acef25cc026a60197e7f108e38e8bf0fedfeea5
Jeff Greer <greergan@gmail.com>
2026-06-24 17:17:05 -0700

    refactor: now using updated Http libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,74 @@
-#include <format>
+#include <algorithm>
 #include <string>
 #include <string_view>
+
+#include <slim/common/http/error_codes.h>
 #include <slim/common/http/headers.h>
 #include <slim/common/http/request.h>
 #include <slim/common/http/url.h>
+#include <slim/common/utilities.h>
 
-using namespace slim::common;
-using namespace slim::common::http;
+namespace slim::common::http {
 
-slim::common::http::Request::Request() {}
+namespace {
+    using slim::common::utilities::iequals;
+} // namespace
 
-slim::common::http::Request::Request(std::string_view _string) {
-	slim::SlimValue results = URL::can_parse(_string);
-	if(results) {
-		__url = URL(_string, results);
-		set_headers();
-	}
+Request::Request(std::string_view s) {
+    hints_map hints;
+
+    auto error = URL::can_parse(s, hints);
+    if (error != ErrorStatus::OK) throw UrlParseException(error);
+
+    url_ = URL(s, hints);
+
+    error = set_headers();
+    if (error != ErrorStatus::OK) throw HttpHeaderException(error);
 }
 
-slim::common::http::Request::Request(const URL& _url) {
-	__url = _url;
-	set_headers();
+ErrorStatus Request::set_headers() {
+    if (iequals(url_.protocol(), "http") || iequals(url_.protocol(), "https")) {
+        auto error = headers_.set("Host", url_.host());
+        if (error != ErrorStatus::OK) return error;
+        return headers_.set("Origin", url_.origin());
+    }
+    return ErrorStatus::OK;
 }
 
-const storage_container& slim::common::http::Request::body() const {
-	return __body;
+std::string Request::serialize() const {
+    constexpr std::size_t initial_buffer_size = 16384;
+
+    const bool add_body = !body_.empty() && method_ != "GET" && method_ != "DELETE";
+
+    std::string pathname{url_.pathname()};
+    if (pathname.empty()) pathname = "/";
+
+    std::string result;
+    result.reserve(initial_buffer_size);
+
+    result += method_;
+    result += " ";
+    result += pathname;
+    result += " ";
+    result += version_;
+    result += "\r\n";
+
+    if (add_body && !headers_.has("Content-Length")) {
+        result += "Content-Length: ";
+        result += std::to_string(body_.size());
+        result += "\r\n";
+    }
+    if (!headers_.has("Content-Type")) {
+        result += "Content-Type: text/plain; charset=utf-8\r\n";
+    }
+
+    result += headers_.serialize();
+
+    if (add_body) {
+        result.insert(result.end(), body_.begin(), body_.end());
+    }
+
+    return result;
 }
 
-const slim::ErrorInfo& slim::common::http::Request::get_error() const {
-	return __error_info;
-}
-
-bool slim::common::http::Request::has_error() const {
-	return __error_info.has_error();
-}
-
-const Headers& slim::common::http::Request::headers() const {
-	return __headers;
-}
-
-std::string_view slim::common::http::Request::method() const {
-	return __method;
-}
-
-void slim::common::http::Request::set_headers() {
-	std::string protocol_lower{__url.protocol()};
-	std::transform(protocol_lower.begin(), protocol_lower.end(), protocol_lower.begin(), [](unsigned char c){ return std::tolower(c); });
-	if(protocol_lower == "http" || protocol_lower == "https") {
-		slim::SlimValue results = __headers.set("Host", __url.host());
-		if(results) {
-			results = __headers.set("Origin", __url.origin());
-		}
-		if(results.has_error()) {
-			__error_info = results.get_error();
-		}
-	}
-}
-
-const URL& slim::common::http::Request::url() const {
-	return __url;
-}
-
-std::string_view slim::common::http::Request::version() const {
-	return __version;
-}
-
-const std::string slim::common::http::Request::to_string() const {
-	bool add_body = (__body.size() > 0 && __method != "GET" && __method != "DELETE") ? true : false;
-	bool has_content_length = false;
-	bool has_content_type = false;
-	std::string pathname = __url.pathname();
-	if(pathname.length() == 0) {
-		pathname = "/";
-	}
-	std::string string_value = std::format("{} {} {}\r\n", __method, pathname, __version);
-
-	for(const auto& [key,value] : __headers.entries() ) {
-		string_value += std::format("{}: {}\r\n", key, value);
-		std::string key_lower{key};
-		std::transform(key_lower.begin(), key_lower.end(), key_lower.begin(), [](unsigned char c){ return std::tolower(c); });
-
-		if(add_body) {
-			if(key_lower == "content-length") {
-				has_content_length = true;
-			}
-		}
-		else if(!has_content_type) {
-			if(key_lower == "content-type") {
-				has_content_type = true;
-			}
-		}
-	}
-
-	if(add_body && !has_content_length) {
-		string_value += std::format("{}: {}\r\n", "Content-Length", std::to_string(__body.size()));
-	}
-
-	if(!has_content_type) {
-		string_value += std::format("{}: {}\r\n", "Content-Type", "text/plain; charset=utf-8");
-	}
-
-	string_value += "\r\n\r\n";
-
-	if(add_body) {
-		string_value += std::format("{}", (char*)__body.data());
-	}
-	return string_value;
-}
-
+} // namespace slim::common::http
